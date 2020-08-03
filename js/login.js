c6e67a0e7ada910326736263305fb88786cb12dd
Salvador Recuero <salvarecuero@gmail.com>
2020-08-03 16:39:20 -0300

    Agrego de login y validación, muestra de productos

--- js/login.js
+++ js/login.js
@@ -1,6 +1,24 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
+const errorMsg = document.getElementById("login-error");
+
+document.forms["login"].addEventListener("submit", checkDatos);
+
+function checkDatos(event){
+    event.preventDefault()
+    var correo = document.forms["login"]["email"].value;
+    var password = document.forms["login"]["password"].value;
+
+    if(!correo || !password){
+        showError();
+    }else{
+        window.location.replace("home.html");
+    }
+}
+
+function showError(){
+    errorMsg.classList.add("alert", "alert-danger");
+    errorMsg.innerHTML = "Correo y/o contraseña vacíos, por favor ingrese sus datos correctamente.";
+}
--- js/products.js
+++ js/products.js
@@ -1,6 +1,40 @@
+var productsArray = [];
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function () {
+    getJSONData(PRODUCTS_URL).then(function (products){
+        if(products.status === "ok"){
+            productsArray = products.data;
+            showProductsList(productsArray);
+        }
+    })
 });
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                        <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
+                    </div>
+                    ${product.description}
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
